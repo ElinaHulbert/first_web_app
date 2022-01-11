cfe779739b4f8f43c007704bda8ae52ba08c9155
ElinaHulbert <92092750+ElinaHulbert@users.noreply.github.com>
2022-01-11 22:24:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App.js";
+import './index.css';
+
+
+
+// ReactDOM.render(
+//   React.createElement("div", {style: {color: "red"}}, null, "Hello!"), //creating element
+//   document.getElementById('root') //rendering to root 
+// );
+
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById("root")
+)
+
+
+// ReactDOM.render(
+//   React.createElement("div", {style: {color: "blue"}}, React.createElement("h1", null, "hi!")), 
+//   document.getElementById('root') 
+// );
