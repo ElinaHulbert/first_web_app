ab6d9a707e2ae1b9774fb48db5e389ccd12dd35a
koltrastH <elina.hulbert@hyperisland.se>
2022-01-17 15:58:11 +0100

    update

--- src/App.js
+++ src/App.js
@@ -33,38 +33,30 @@ import {Description} from "./pages";
 
 
 
-function Audio({channel}){
-  // const ping = "https://sverigesradio.se/topsy/direkt/srapi/5283.mp3";
-  // const [play] = useSound(watersound);
-  const source = [channel.liveaudio.url];
-  function playSound() {
-    var stream = new Howl({
-      src: [source],
-      ext: ['mp3'],
-      autoplay: true,
-      html5: true
-    });
-  stream.play()
-  }
-  return source.map((sound) => {
-    return(
-      <>
-        <button onClick={playSound()}>{channel.id}</button>
-        {/* <audio src={channel.liveaudio.url}></audio> */}
-      </>
-  )})
+// function Audio({channel}){
+//   // const ping = "https://sverigesradio.se/topsy/direkt/srapi/5283.mp3";
+//   // const [play] = useSound(watersound);
+//   const source = [channel.liveaudio.url];
+  
+//   return source.map((sound) => {
+//     return(
+//       <>
+       
+//         {/* <audio src={channel.liveaudio.url}></audio> */}
+//       </>
+//   )})
   
   
-}
+// }
 
 
-function Channel({channel}){
-  console.log("channelcomp", channel);
+function Channel({channel, onClick}){
+  // console.log("channelcomp", channel);
   return(
     <>
       <h2>{channel.name}</h2>
       <img src={channel.image} alt="alt"></img>
-      <Audio channel={channel}/>
+      <button onClick={onClick}>{channel.id}</button>
     </>
   )
 
@@ -75,9 +67,9 @@ function Channel({channel}){
 
 function Fetch(){
 
-  
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [selectedChannel, setSelectedChannel] = useState(null);
 
   useEffect(() => {
     getData();
@@ -90,8 +82,22 @@ function Fetch(){
     .then (setData)
     .catch (setError);
 }
+
+
+var stream = new Howl({
+  src: [selectedChannel.liveaudio.url],
+  ext: ['mp3'],
+  autoplay: true,
+  html5: true
+});
+
+
+
+  function playSound() {
+    stream.play()
+  }
   
-  console.log(data)
+  console.log("data ", data)
 
   
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
@@ -102,7 +108,7 @@ function Fetch(){
       <>
         
         <ul>
-          {data.channels.map((channel) =><li key={channel.id}><Channel channel={channel}/><Link to={`/channel/${channel.id}`}>{channel.name}</Link></li>)}
+          {data.channels.map((channel) =><li key={channel.id}><Channel onClick={()=>{setSelectedChannel(channel); console.log("selectedChannel ", selectedChannel)}} channel={channel}/><Link to={`/channel/${channel.id}`}>{channel.name}</Link></li>)}
         </ul>
       </>
   );
