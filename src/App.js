07406407ddcf5fd03d85fb40dbafe236590364a6
koltrastH <elina.hulbert@hyperisland.se>
2022-01-19 02:57:07 +0100

    fixed the pages, transferred tagline to separate pages, added channels' urls

--- src/App.js
+++ src/App.js
@@ -1,62 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import useSound from 'use-sound';
-import watersound from "./watersound.mp3";
-import {Howl, Howler} from "howler";
 import {Routes, Route, Link} from "react-router-dom";
 import {Description} from "./pages";
 
-//   var playing = false;
-
-//   function play() {
-//     load();
-//     play();
-//     playing = true;
-//   }
-
-//   function pause() {
-//    pause();
-//     playing = false;
-//   }
-
-// function PlayOnClick(){
-//   if(!playing) {
-//     play();
-// } else {
-//     pause();
-// }
-// }
-
-// const audio = new Audio("https://sverigesradio.se/topsy/direkt/srapi/5283.mp3");
-// audio.play()
-// console.log("audio", audio);
 
 
-
-// function Audio({channel}){
-//   // const ping = "https://sverigesradio.se/topsy/direkt/srapi/5283.mp3";
-//   // const [play] = useSound(watersound);
-//   const source = [channel.liveaudio.url];
-  
-//   return source.map((sound) => {
-//     return(
-//       <>
-       
-//         {/* <audio src={channel.liveaudio.url}></audio> */}
-//       </>
-//   )})
-  
-  
-// }
-
-
-function Channel({channel, onClick}){
-  // console.log("channelcomp", channel);
+function Channel({channel}){
   return(
     <>
+    <br></br><br></br><br></br>
       <h2>{channel.name}</h2>
       <img src={channel.image} alt="alt"></img>
-      <button onClick={onClick}>{channel.id}</button>
+      <audio controls={true} allow="autoplay"><source src={channel.liveaudio.url}  type="audio/mpeg"></source></audio>
     </>
   )
 
@@ -65,11 +20,27 @@ function Channel({channel, onClick}){
 
 
 
-function Fetch(){
+function Fetch({data}){
+  console.log("data ", data)
+    return (
+      <>
+        <ul>
+          {data.channels.map((channel) =><li key={channel.id}><Channel 
+           channel={channel}/><Link to={`/channel/${channel.id}`}>{channel.name}</Link></li>)}
+        </ul>
+      </>
+  );
 
+
+}
+
+
+
+
+
+function App() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  const [selectedChannel, setSelectedChannel] = useState(null);
 
   useEffect(() => {
     getData();
@@ -82,50 +53,15 @@ function Fetch(){
     .then (setData)
     .catch (setError);
 }
-
-
-var stream = new Howl({
-  src: [selectedChannel.liveaudio.url],
-  ext: ['mp3'],
-  autoplay: true,
-  html5: true
-});
-
-
-
-  function playSound() {
-    stream.play()
-  }
+if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+if (!data) return null;
   
-  console.log("data ", data)
-
-  
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (!data) return null;
-
-
-    return (
-      <>
-        
-        <ul>
-          {data.channels.map((channel) =><li key={channel.id}><Channel onClick={()=>{setSelectedChannel(channel); console.log("selectedChannel ", selectedChannel)}} channel={channel}/><Link to={`/channel/${channel.id}`}>{channel.name}</Link></li>)}
-        </ul>
-      </>
-  );
-
-
-}
-
-
-
-
-
-function App({channel}) {
   return (
+    
     <div className='App'>
       <Routes>
-        <Route path="/" element={<Fetch />}/>
-        <Route path="/channel/:id" element={<Description channel={channel}/>}/>
+        <Route path="/" element={<Fetch data={data}/>}/>
+        <Route path="/channel/:id" element={<Description data={data}/>}/>
       </Routes>
       
     </div>
--- src/pages.js
+++ src/pages.js
@@ -1,25 +1,44 @@
 import React from "react";
-import {Link} from "react-router-dom";
 
 
-// export function Home(){
-//     return(
-//         <div>
-//             <p>Home</p>
-//             <nav>
-//                 <Link to="description">Description</Link>
-//             </nav>
-//         </div>
-//     )
-// }
 
 
-export function Description(props){
-    console.log(props.channel.name)
+export function Description({data}){
+   
     return(
-        
+       
         <div>
-            <p>{props.channel.name}</p>
+        <ul>
+            {data.channels.map((channel) => console.log(channel))}
+                        
+        </ul>
+        <Tagline data={data}/>
+        {console.log("data in description", data)}
         </div>
+        
     )
+}
+
+function Tagline({data}){
+    let href = document.location.href;
+    let result = href.slice(30,).toString();
+    console.log("data", data)
+    return data.channels.map((channel)=>{
+        console.log(channel.id)
+        console.log("result length:", result.length, " result is:", result)
+        if (result==channel.id){
+                return(
+                    <>
+                    <h1>{channel.name}</h1>
+                    <h3>Channeltype: {channel.channeltype}</h3>
+                    <p>Tagline: {channel.tagline}</p>
+                    <a href={channel.siteurl}>Site URL</a>
+
+                    </>
+                   
+                )
+        }
+        return(null)
+    })
+     
 }
