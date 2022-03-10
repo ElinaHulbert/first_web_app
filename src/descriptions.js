0c5d4aea2bc21c18e59d16e4f9cc919040709ce9
koltrastH <elina.hulbert@hyperisland.se>
2022-03-10 11:37:27 +0100

    netlify issues

--- src/descriptions.js
+++ src/descriptions.js
@@ -1,39 +1,32 @@
 import React from "react";
 
-
-
-
-export function Description({data}){
-    return(
-        <div>
-        <ul>
-            {data.channels.map((channel) => console.log(channel))}
-                        
-        </ul>
-        <Tagline data={data}/>
-        {console.log("data in description", data)}
-        </div>
-    )
+export function Description({ data }) {
+  return (
+    <div>
+      {/* <ul><li key={data.id}>{data.channels.map((channel) => console.log(channel))}</li></ul> */}
+      <Tagline data={data} />
+      {console.log("data in description", data)}
+    </div>
+  );
 }
 
-function Tagline({data}){
-    let href = document.location.href;
-    let result = href.slice(30,).toString();
-    console.log("data", data)
-    return data.channels.map((channel)=>{
-        console.log(channel.id)
-        console.log("result length:", result.length, " result is:", result)
-        if (result==channel.id){
-                return(
-                    <div style={{backgroundColor: `#${channel.color}`}}>
-                    <h1>{channel.name}</h1>
-                    <h3>Channeltype: {channel.channeltype}</h3>
-                    <p>Tagline: {channel.tagline}</p>
-                    <a href={channel.siteurl}>Site URL</a>
-                    </div>
-                )
-        }
-        return(null)
-    })
-     
+function Tagline({ data }) {
+  let href = document.location.href;
+  let result = href.slice(30).toString();
+  console.log("data", data);
+  return data.channels.map((channel) => {
+    console.log(channel.id);
+    console.log("result length:", result.length, " result is:", result);
+    if (result == channel.id) {
+      return (
+        <div key={channel.id} style={{ backgroundColor: `#${channel.color}` }}>
+          <h1>{channel.name}</h1>
+          <h3>Channeltype: {channel.channeltype}</h3>
+          <p>Tagline: {channel.tagline}</p>
+          <a href={channel.siteurl}>Site URL</a>
+        </div>
+      );
+    }
+    return null;
+  });
 }
