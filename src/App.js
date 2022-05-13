aa8a00cb403fa725555d5f0131eccb23f05f80d7
koltrastH <elina.hulbert@hyperisland.se>
2022-05-13 15:55:13 +0200

    defined width for cards, changed backgrounds, added header

--- src/App.js
+++ src/App.js
@@ -1,62 +1,56 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import {Routes, Route, Link} from "react-router-dom";
-import {Description} from "./descriptions";
-import {Extra} from "./extra";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { Description } from "./Description/Descriptions";
+import { Extra } from "./Extra/extra";
+import Header from "./Header/Header";
 
-
-
-function Channel({channel}){
-  return(
+function Channel({ channel }) {
+  return (
     <>
-<div class="container-02">
-		
-		<div class="glassmorphic-card">
-			<div class="imgBox">
-        <img className="images" src={channel.image} alt="alt"></img>
-			</div>
-			<div class="contentBox">
-        <h2>{channel.name}</h2>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ...</p>
-        <audio className="audio" controls={true} allow="autoplay"><source src={channel.liveaudio.url}  type="audio/mpeg"></source></audio>s
-			</div>
-		</div>
-		
-</div>
+      <div className="container-02">
+        <div className="glassmorphic-card">
+          <div className="imgBox">
+            <img className="images" src={channel.image} alt="alt"></img>
+          </div>
+          <div className="contentBox">
+            <h2>{channel.name}</h2>
+            <p>{channel.tagline}</p>
+            <audio className="audio" controls={true} allow="autoplay">
+              <source src={channel.liveaudio.url} type="audio/mpeg"></source>
+            </audio>
+            <Link className="links" to={`/channel/${channel.id}`}>
+              Description: {channel.name}
+            </Link>
+          </div>
+        </div>
+      </div>
 
-
-
-
-    <br></br><br></br><br></br>
-     
-      
-      
+      <br></br>
+      <br></br>
+      <br></br>
     </>
-  )
-
-}
-
-
-
-
-function Fetch({data}){
-  console.log("data ", data)
-    return (
-      <>
-      <Link className="extralink" to="extra">Extra</Link>
-        <ul className="ul">
-          {data.channels.map((channel) =><li className="name" key={channel.id}><Channel 
-           channel={channel}/><Link className="links" to={`/channel/${channel.id}`}>Description of the channel: {channel.name}</Link></li>)}
-        </ul>
-      </>
   );
-
-
 }
 
-
-
-
+function Fetch({ data }) {
+  console.log("data ", data);
+  return (
+    <>
+      <Header></Header>
+      <ul className="ul">
+        {data.channels.map((channel) => (
+          <li className="list" key={channel.id}>
+            <Channel channel={channel} />
+          </li>
+        ))}
+      </ul>
+      <Link className="extralink" to="extra">
+        Extra
+      </Link>
+    </>
+  );
+}
 
 function App() {
   const [data, setData] = useState(null);
@@ -66,29 +60,26 @@ function App() {
     getData();
   }, []);
 
+  function getData() {
+    fetch(
+      "https://api.sr.se/api/v2/channels?format=json&indent=true&pagination=false"
+    )
+      .then((response) => response.json())
+      .then(setData)
+      .catch(setError);
+  }
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (!data) return null;
 
- function getData(){ 
-   fetch ('https://api.sr.se/api/v2/channels?format=json&indent=true&pagination=false')
-    .then (response => response.json())
-    .then (setData)
-    .catch (setError);
-}
-if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-if (!data) return null;
-  
   return (
-    
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route path="/" element={<Fetch data={data}/>}/>
-        <Route path="/channel/:id" element={<Description data={data}/>}/>
-        <Route path="/extra" element={<Extra/>}/>
+        <Route path="/" element={<Fetch data={data} />} />
+        <Route path="/channel/:id" element={<Description data={data} />} />
+        <Route path="/extra" element={<Extra />} />
       </Routes>
-      
     </div>
-  )
+  );
 }
-
-
 
 export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="top_menu">
+        <h1>Swedish Radio</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
